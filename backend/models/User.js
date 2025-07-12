79c965be2243537aad2bc17e0fb9e9894441bd9d
PedroTaskal <andreanoantonio67@gmail.com>
2025-07-13 00:46:54 +0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
 // backend/models/User.js
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs'); // Більше не потрібен, якщо не хешуємо
 
 const UserSchema = new mongoose.Schema({
     email: {
@@ -16,28 +16,28 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Будь ласка, вкажіть пароль'],
         minlength: [6, 'Пароль повинен бути не менше 6 символів'],
-        select: false // Не повертати пароль при запитах, якщо явно не вказано
+        // select: false // Можна прибрати, якщо пароль не хешується і ви хочете його повертати за замовчуванням
+                        // Або залишити, якщо не хочете, щоб він повертався при звичайних запитах
     },
     createdAt: {
         type: Date,
         default: Date.now
     }
-    // Можна додати інші поля: username, profileData, telegramId тощо
 });
 
-// Middleware для хешування пароля перед збереженням
-UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
+// !!! УВАГА: ЗАКОМЕНТОВАНО/ВИДАЛЕНО ЛОГІКУ ХЕШУВАННЯ ПАРОЛІВ !!!
+// UserSchema.pre('save', async function (next) {
+//     if (!this.isModified('password')) {
+//         next();
+//     }
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+// });
 
-// Метод для порівняння введеного пароля з хешованим
-UserSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-};
+// !!! УВАГА: ЗАКОМЕНТОВАНО/ВИДАЛЕНО МЕТОД ПОРІВНЯННЯ ХЕШОВАНИХ ПАРОЛІВ !!!
+// UserSchema.methods.matchPassword = async function (enteredPassword) {
+//     return await bcrypt.compare(enteredPassword, this.password);
+// };
 
 module.exports = mongoose.model('User', UserSchema);
